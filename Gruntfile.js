c338596835d7d9029d5008e5710b18df68d1d264
Rodrigo Morais <morais.rm@gmail.com>
2013-09-13 20:48:29 -0300

    Adicionando QUnit no Grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,21 @@
 module.exports = function (grunt) {
     grunt.initConfig({
-        
+        qunit_junit: {
+            options: {
+                dest:'scripts/tests/reports',
+                namer: function (url) {
+                    return 'qunit.test.report';
+                }
+            }
+        },
+        qunit: {
+            all: ['index_grunt.htm']
+        },
     });
 
+	grunt.registerTask('test', ['qunit_junit', 'qunit']);
 
     // carrega plugins
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-qunit-junit');
 }; 
